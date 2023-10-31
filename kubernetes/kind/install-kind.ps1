e5b7ad332a39a0728cf5ce17eb1c18f10149e773
Sebastian Szuber <sebastian.szuber@nordcloud.com>
2023-10-31 14:29:36 +0100

    Add a script to install kind

---
+++ kubernetes/kind/install-kind.ps1
@@ -0,0 +1,20 @@
+
+# Find latest versions number based on tags on remote repo
+$regex = '([0-9]+.[0-9]+.[0-9]+$)'
+$version = git ls-remote --tags https://github.com/kubernetes-sigs/kind.git | Select-String $regex -AllMatches | Select-Object -Expand Matches | ForEach-Object { $_.Value } `
+    | ForEach-Object {[System.Version]$_} | Sort-Object | Select-Object -Last 1 | ForEach-Object {[string] $_}
+Write-Output "Latest version found: $version"
+# $version = "v0.20.0"
+
+# Define target path and download selected binary version
+$addPath = "c:\dev"
+Invoke-WebRequest -Uri https://kind.sigs.k8s.io/dl/v$version/kind-windows-amd64 -OutFile  c:\dev\kind.exe
+
+# Add target path to the environment if not exists
+if (Test-Path $addPath){
+    $regexAddPath = [regex]::Escape($addPath)
+    $arrPath = $env:Path -split ';' | Where-Object {$_ -notMatch "^$regexAddPath\\?"}
+    $env:Path = ($arrPath + $addPath) -join ';'
+} else {
+    Throw "'$addPath' is not a valid path."
+}
